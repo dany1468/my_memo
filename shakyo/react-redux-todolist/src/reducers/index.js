962deb23ad335328993bd570f7897c5a418f80b9
dany1468 <dany1468@gmail.com>
2017-10-02 16:18:51 +0900

    Can delete a todo

--- shakyo/react-redux-todolist/src/reducers/index.js
+++ shakyo/react-redux-todolist/src/reducers/index.js
@@ -19,6 +19,15 @@ export const reducer = (state = initialState, action) => {
         ],
       };
 
+    case types.DELETE_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos.filter(todo => (
+            todo.id !== action.id
+          )),
+        ],
+      };
     default:
       return state;
   }
